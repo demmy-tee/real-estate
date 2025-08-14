e3db6a4ef286466015e16f21555c1dd82576bf37
demmy-tee <danielayo654@gmail.com>
2025-08-14 12:56:22 +0100

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Real Estate API');
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("MongoDB connection error:", error);
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
