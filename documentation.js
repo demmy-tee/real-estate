b8cce9c82460427a4a2eeff9b65c75ef56302fad
demmy-tee <danielayo654@gmail.com>
2025-08-14 02:44:27 +0100

    server was created

---
+++ documentation.js
@@ -0,0 +1,15 @@
+/* 
+Documentation on how the mern-stack app was built
+
+first install react and tailwind,
+install react-router-dom
+create components and pages
+install react-icons from npm
+link from react router dom helps to switch within pages without reloading the page
+// dynamic object assignment
+//useNavigate from react-router-dom
+useEffect is used for get request not post request
+// redux query
+//
+
+*/
